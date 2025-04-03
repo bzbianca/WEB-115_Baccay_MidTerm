3f45a6146bba94ae11f3dfcae08aa08c394228ef
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-04-03 12:09:32 -0400

    finsihed

--- codingExam.js
+++ codingExam.js
@@ -1,7 +1,7 @@
 
 const Button1 = document.getElementById("Button1")
 
-const fixedInterest = .575
+const fixedInterest = .0575
 Button1.addEventListener("click", function() {
     let oldLoanAmount = Number(prompt("Enter the loan amount:"))
     let downpayment = Number(prompt("Enter the downpayment percentage (%):"))
@@ -15,26 +15,34 @@ Button1.addEventListener("click", function() {
 let counter = 0
 function calculate(loanAmount, fixedInterest, loanTerm) { 
     let newLoan = loanAmount
+    let display = document.createElement("p")
+    display.innerHTML = "Loan Term: " + loanTerm + ", Annual Interest rate: " + fixedInterest + "Principal loan amount: " + loanAmount
     while (newLoan !== 0) {
         counter += 1;
-        let monthlyPayment = ((loanAmount * fixedInterest) / 1 - (1 + fixedInterest) ** -loanTerm).toFixed(2)
-        let interestAmount = (loanAmount * fixedInterest / 12)
         let principalAmount = monthlyPayment - interestAmount
-        let interestPaid = (loanAmount - monthlyPayment) * fixedInterest
-        let principalPaid = (monthlyPayment - principalAmount)
         newLoan = newLoan - principalAmount
+        let monthlyPayment = ((monthlyInterestRate * newLoan) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))).toFixed(2);
+        let interestAmount = (newLoan * fixedInterest / 12)
+        let interestPaid = (newLoan - monthlyPayment) * fixedInterest
+        let principalPaid = (monthlyPayment - principalAmount)
+        let monthTerm = loanTerm * 12
 
-        let termMortgage = document.createElement("p")
-        termMortgage.innerHTML("Month " + counter)
         
-        let interestMortgage = document.createElement("p")
-        interestMortgage.innerHTML(interestAmount)
 
-        let amountMortgage = document.createElement("p")
-        amountMortgage.innerHTML(newLoan + interestAmount)
+        let displayCal = document.createElement("p")
+        displayCal.innerHTML = "Monthly Payment: " + monthlyPayment + "Interest Paid: " + interestAmount + "Principal paid: " + principalPaid + "Remaining Loan: " + newLoan
 
-        let Totalmortgage = document.createElement("p")
-        Totalmortgage.innerHTML()
+        // let termMortgage = document.createElement("p")
+        // termMortgage.innerHTML = ("Month " + counter + ", Interest Amount: " + interestAmount, )
+        
+        // let interestMortgage = document.createElement("p")
+        // interestMortgage.innerHTML = (interestAmount)
+
+        // let amountMortgage = document.createElement("p")
+        // amountMortgage.innerHTML = (newLoan + interestAmount)
+
+        // let Totalmortgage = document.createElement("p")
+        // Totalmortgage.innerHTML = ()
 
 
     }
